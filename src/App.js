2772bcf08fce3efe5d5d5f5122d2c06a94f5c873
Diego Maceda <dmaceda2288@gmail.com>
2022-11-01 20:31:10 -0300

    list component added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Listado from "./components/Listado";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import "./css/bootstrap.min.css"
+
 
 
 function App() {
   return (
     <>
-    <Header />
+    {/* <Header /> */}
+
+    <div className='container mt-3'>
     <Routes>
       <Route path="/" element={<Login />}/>
       <Route exact path="/listado" element={<Listado/>}/>
     </Routes>
-    <Footer />
+    </div>
+    {/* <Footer /> */}
     </>
     
   );
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,37 +1,51 @@
-import React, { useEffect }from 'react';
+import React, { useEffect, useState }from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
 
 const Listado = () => {
-    const movieS = [1, 2, 3]
-
-   
+    const API = 'a9f89e28a9c52e268490a138c8218858'
 
     const navigate = useNavigate();
+    const [moviesList, setMoviesList] = useState([]);
+
     useEffect(() => {
         const token = localStorage.getItem('token');
            
         if (token === null ) {
             navigate('/')
         }
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=a9f89e28a9c52e268490a138c8218858&language=en-ES&page=1'
+        axios.get(endPoint)
+            .then(resp => {
+                const apiData = resp.data.results
+                setMoviesList(apiData)
+            })
+    }, [setMoviesList]);
+
+    console.log(moviesList)
    
   return (
-    <div>
-        {
+        <div className="row">
+            <div className="col-3">
+            <div className="card" >
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <Link to="/" className="btn btn-primary">Go somewhere</Link>
+                </div>
+                </div>
+            </div>
 
-            movieS.map((movie, id) => {
-                (
-                    <div key={id}>
-                        <h5>{movie.title}</h5>
-                    </div>
-                )
-            })
-        }
-        <div>Listado</div>
-    </div>
+            
+        </div>
   )
 }
 
