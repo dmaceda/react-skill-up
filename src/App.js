caf4a9e955211ac79f3f35015517342b3c3d9cc0
Diego Maceda <dmaceda2288@gmail.com>
2022-11-01 23:34:17 +0100

    footer added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Routes ,Route } from 'react-router-dom';
 import Login from "./components/Login";
 import Listado from "./components/Listado";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
-    
+    <>
+    <Header />
     <Routes>
       <Route path="/" element={<Login />}/>
       <Route exact path="/listado" element={<Listado/>}/>
     </Routes>
+    <Footer />
+    </>
     
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <nav>
+            <ul>
+                <li>
+                    <a href="" rel='noopener noreferrer'>IG</a>
+                </li>
+                <li>
+                    <a href="" rel='noopener noreferrer'>Linkedin</a>
+                </li>
+            </ul>
+        </nav>
+        <p>Copyright Alkemy Challenge 2022</p>
+    </footer>
+  )
+}
+
+export default Footer
